bb3551d8a928ae4a2d89d521c1901be5cfa08a70
7DedSins <55441163+7DedSins@users.noreply.github.com>
2026-09-06 10:09:58 +0530

    refeactor: split Django settings into base and local

---
+++ config/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ti$3&di1#br2hr*x&w_ihur938k1%hnwul(#wl4l8vi*$3gq30"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
